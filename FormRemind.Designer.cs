006f3ccd2296f76ee8cad60f9ab2288490585c05
chishin <pro@xxgzs.org>
2020-05-25 21:47:12 +0800

    增加提示窗体文件 版本 V0.0.0.11

---
+++ FormRemind.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TimedRemindTool
+{
+    partial class FormRemind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.timerForm = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelMessage.Location = new System.Drawing.Point(12, 49);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(380, 35);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = " 定时提醒事件【】";
+            this.labelMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerForm
+            // 
+            this.timerForm.Interval = 10;
+            this.timerForm.Tick += new System.EventHandler(this.timerForm_Tick);
+            // 
+            // FormRemind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 141);
+            this.Controls.Add(this.labelMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormRemind";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "定时提醒";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormRemind_FormClosed);
+            this.Load += new System.EventHandler(this.FormRemind_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Timer timerForm;
+    }
+}
---
+++ FormRemind.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimedRemindTool
+{
+    public partial class FormRemind : Form
+    {
+        private String strMessage;
+        private const int POS = 3;
+        private int timIndex = 0;
+        private int x;
+        private int y;
+
+        public FormRemind(String strMessage)
+        {
+            this.strMessage = " 定时提醒事件【" + strMessage+ " 】";
+            InitializeComponent();
+        }
+
+        private void FormRemind_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2 - this.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 - this.Height / 2);
+            x = this.Left;
+            y = this.Top;
+            this.labelMessage.Text = this.strMessage;
+            timerForm.Start();
+        }
+
+        private void timerForm_Tick(object sender, EventArgs e)
+        {
+            if (++timIndex < 40)
+            {
+                switch (timIndex % 10)
+                {
+                    case 0:
+                        this.Location = new Point(x - POS, y);
+                        break;
+                    case 1:
+                        this.Location = new Point(x - POS, y - POS);
+                        break;
+                    case 2:
+                        this.Location = new Point(x, y - POS);
+                        break;
+                    case 3:
+                        this.Location = new Point(x + POS, y - POS);
+                        break;
+                    case 4:
+                        this.Location = new Point(x + POS, y);
+                        break;
+                    case 5:
+                        this.Location = new Point(x + POS, y + POS);
+                        break;
+                    case 6:
+                        this.Location = new Point(x, y + POS);
+                        break;
+                    case 7:
+                        this.Location = new Point(x - POS, y + POS);
+                        break;
+                    case 8:
+                        this.Location = new Point(x - POS, y);
+                        break;
+                    case 9:
+                    default:
+                        this.Location = new Point(x, y);
+                        break;
+                }
+            }
+            else
+            {
+                timerForm.Stop();
+            }
+        }
+
+        private void FormRemind_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
