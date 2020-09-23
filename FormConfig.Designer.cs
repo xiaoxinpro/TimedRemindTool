7350b23ed90caed73783f8eb8adeca78e717e9a9
chishin <pro@xxgzs.org>
2020-09-23 13:34:12 +0800

    初始化配置页面

--- FormConfig.Designer.cs
+++ FormConfig.Designer.cs
@@ -29,19 +29,30 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConfig));
+            this.groupBoxDefault = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // groupBoxDefault
+            // 
+            this.groupBoxDefault.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxDefault.Name = "groupBoxDefault";
+            this.groupBoxDefault.Size = new System.Drawing.Size(360, 101);
+            this.groupBoxDefault.TabIndex = 0;
+            this.groupBoxDefault.TabStop = false;
+            this.groupBoxDefault.Text = "全局配置";
             // 
             // FormConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.groupBoxDefault);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormConfig";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormConfig";
+            this.Text = "设置";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormConfig_FormClosing);
             this.Load += new System.EventHandler(this.FormConfig_Load);
             this.ResumeLayout(false);
@@ -49,5 +60,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxDefault;
     }
 }
